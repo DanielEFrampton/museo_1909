0a2ccfbccb8dfae7ce2156a8740aa303e458e379
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-30 09:20:36 -0600

    Write test and code to instantiate Curator

---
+++ test/curator_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/photograph'
+require './lib/artist'
+require './lib/curator'
+
+class CuratorTest < Minitest::Test
+
+  def setup
+    @curator = Curator.new
+  end
+
+  def test_it_exists
+    assert_instance_of Curator, @curator
+  end
+
+  def test_it_initializes_with_attributes
+    assert_equal [], @curator.photographs
+  end
+end
