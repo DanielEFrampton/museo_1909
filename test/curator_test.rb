c08c5f01abf11fef6c6978e961f922a82f5817a4
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-30 09:25:34 -0600

    Add test for add_artist

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -20,6 +20,20 @@ class CuratorTest < Minitest::Test
                                  artist_id: "2",
                                  year: "1941"
                               })
+    @artist_1 = Artist.new({
+        id: "1",
+        name: "Henri Cartier-Bresson",
+        born: "1908",
+        died: "2004",
+        country: "France"
+    })
+    @artist_2 = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+    })
   end
 
   def test_it_exists
@@ -28,11 +42,17 @@ class CuratorTest < Minitest::Test
 
   def test_it_initializes_with_attributes
     assert_equal [], @curator.photographs
+    assert_equal [], @curator.artists
   end
 
   def test_it_can_add_photographs
     @curator.add_photograph(@photo_1)
     @curator.add_photograph(@photo_2)
     assert_equal [@photo_1, @photo_2], @curator.photographs
+  end
+
+  def test_it_can_add_artist
+    @curator..add_artist(@artist_1)
+    @curator..add_artist(@artist_2)
   end
 end
