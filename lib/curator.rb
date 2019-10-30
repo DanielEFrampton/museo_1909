f069f04e0303bebc0db01733def6e6d5a6a5d697
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-30 09:25:25 -0600

    Write test-passing add_photographs method

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,7 +1,12 @@
 class Curator
-  attr_reader :photographs
-  
+  attr_reader :photographs, :artists
+
   def initialize
     @photographs = []
+    @artists = []
+  end
+
+  def add_photograph(photo)
+    @photographs << photo
   end
 end
